3730e430846d296117b164b6a3a509a6cf0d691e
turbo1337 <torbam.12d@acsjszki.hu>
2022-03-28 12:47:32 +0200

    Add project files.

---
+++ 2022-03-28_Balkezesek/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022_03_28_Balkezesek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
